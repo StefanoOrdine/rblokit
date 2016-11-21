a34da79094aa3061417ae543f4f7d814ee68d8a1
Stefano Ordine <stefano.ordine@gmail.com>
2016-11-21 22:28:50 +0100

    fatto cose

--- lokit.rb
+++ lokit.rb
@@ -1,17 +1,14 @@
 require 'ffi'
+require 'pry'
 
 module RbLoKit
   extend FFI::Library
 
-  class LibreOfficeKitClass < FFI::Struct
-  end
+  class LibreOfficeKitClass < FFI::Struct; end
+  class LibreOfficeKitDocumentClass < FFI::Struct; end
 
   class LibreOfficeKit < FFI::Struct
     layout :pClass, LibreOfficeKitClass.ptr
-  end
-
-
-  class LibreOfficeKitDocumentClass < FFI::Struct
   end
 
   class LibreOfficeKitDocument < FFI::Struct
@@ -34,37 +31,6 @@ module RbLoKit
   ffi_lib '/usr/lib/libreoffice/program/libmergedlo.so'
   attach_function :libreofficekit_hook, [:string], LibreOfficeKit.ptr
 end
-
-RbLoKit.libreofficekit_hook('/usr/lib/libreoffice/program/')
-
-__END__
-
-typedef struct _LibreOfficeKit LibreOfficeKit;
-typedef struct _LibreOfficeKitClass LibreOfficeKitClass;
-typedef struct _LibreOfficeKitDocument LibreOfficeKitDocument;
-typedef struct _LibreOfficeKitDocumentClass LibreOfficeKitDocumentClass;
-struct _LibreOfficeKit
-{
-    LibreOfficeKitClass* pClass;
-};
-struct _LibreOfficeKitClass
-{
-  size_t  nSize;
-  void                    (*destroy)       (LibreOfficeKit *pThis);
-  LibreOfficeKitDocument* (*documentLoad)  (LibreOfficeKit *pThis, const char *pURL);
-  char*                   (*getError)      (LibreOfficeKit *pThis);
-};
-struct _LibreOfficeKitDocument
-{
-    LibreOfficeKitDocumentClass* pClass;
-};
-struct _LibreOfficeKitDocumentClass
-{
-  size_t  nSize;
-  void (*destroy)   (LibreOfficeKitDocument* pThis);
-  int (*saveAs)     (LibreOfficeKitDocument* pThis,
-                     const char *pUrl,
-                     const char *pFormat,
-                     const char *pFilterOptions);
-};
-LibreOfficeKit *libreofficekit_hook(const char* install_path);
+binding.pry
+lokit = RbLoKit.libreofficekit_hook('/usr/lib/libreoffice/program/')
+doc = lokit.pClass.documentLoad(lokit, 'test.odt')
